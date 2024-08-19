d4212dbd0e6fbdd1506df54957f0412148bde14b
Oğuzhan Savaş <mustafaoguzhan.savas@bilgeadamboost.com>
2024-08-19 15:41:31 +0300

    add appuser

---
+++ IdentityProject.DAL/Concrate/Context.cs
@@ -0,0 +1,23 @@
+﻿using IdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProject.DAL.Concrate
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial Catalog=IdentityProjectnet8Db; integrated Security=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+
+        public DbSet<CustomerAccountProcess> CustomerAccountsProcess { get; set; }
+    }
+}
---
+++ IdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountId { get; set; }
+
+        public string CustomerAccountNumber { get; set; }
+
+        public string Currency { get; set; }
+
+        public decimal CustomerAccountBalance { get; set; }
+
+        public string BankBrach { get; set; }
+
+
+    }
+}
